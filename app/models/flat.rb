164beb15e35f910fc3755b7ed31381fb3ad1e7de
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-19 17:37:55 +0100

    update flat model

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+   geocoded_by :address #decimos la forma en la que vamos a hacerlo basado en la columna 'addres' in my flats table
+  after_validation :geocode, if: :will_save_change_to_address? #API code if you change the address
 end
