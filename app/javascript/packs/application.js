c9465d23f09ea05ff7bd2a1256ff0c0938c27075
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-21 14:21:24 +0100

    map still not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,6 @@ import "bootstrap";
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout! because we import css
 // pay attention to this line original: '../plugins/init_mapbox'
-import { initMapbox } from '../plugins/init_mapbox';
+import { initMapbox } from 'packs/map';
 
 initMapbox()
