bf2e1fd7a8f5c0b32628e889dbedc0a49f61b84b
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-23 23:41:54 +0100

    .

--- app/javascript/packs/application.js
+++
@@ -1,7 +0,0 @@
-import "bootstrap";
-
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout! because we import css
-// pay attention to this line original: '../plugins/init_mapbox'
-import { initMapbox } from 'packs/map';
-
-initMapbox()
