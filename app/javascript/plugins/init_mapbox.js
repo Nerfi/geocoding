34f3660cc3820f8f2e4577d7d5ebb1997342c780
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-24 13:34:53 +0100

    markets zoom working

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,7 +15,19 @@ const initMapbox = () => {
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
   });
+  // con esto conseguimos centrar los markets para que no tengamos nosotros que hacer zoom
+  const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+};
+
+if (mapElement) {
+  // [ ... ]
+  fitMapToMarkers(map, markers);
+}
   }
+
 
 };
 
